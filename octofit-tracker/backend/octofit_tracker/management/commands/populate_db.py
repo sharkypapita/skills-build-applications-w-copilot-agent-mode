ca96c923a8dd5701255de39a4058827e5123e72e
avcabalo <avcabalo@gmail.com>
2025-11-27 08:19:06 +0000

    Add Django management command to populate test data for octofit_db

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Teams
+        marvel = Team.objects.create(name='marvel')
+        dc = Team.objects.create(name='dc')
+
+        # Users
+        ironman = User.objects.create(email='ironman@marvel.com', name='Iron Man', team='marvel')
+        captain = User.objects.create(email='captain@marvel.com', name='Captain America', team='marvel')
+        batman = User.objects.create(email='batman@dc.com', name='Batman', team='dc')
+        superman = User.objects.create(email='superman@dc.com', name='Superman', team='dc')
+
+        # Activities
+        Activity.objects.create(user='Iron Man', type='run', duration=30)
+        Activity.objects.create(user='Captain America', type='swim', duration=45)
+        Activity.objects.create(user='Batman', type='cycle', duration=60)
+        Activity.objects.create(user='Superman', type='fly', duration=120)
+
+        # Leaderboard
+        Leaderboard.objects.create(team='marvel', points=75)
+        Leaderboard.objects.create(team='dc', points=90)
+
+        # Workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        Workout.objects.create(name='Situps', description='Do 30 situps')
+        Workout.objects.create(name='Squats', description='Do 40 squats')
+
+        self.stdout.write(self.style.SUCCESS('Test data populated successfully.'))
